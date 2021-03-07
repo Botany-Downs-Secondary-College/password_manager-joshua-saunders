2bcd42e3e6d28b5acf25fc4313f5f0328f21a407
JoshuaSaunders2 <78829864+Joshua-Saunders@users.noreply.github.com>
2021-03-07 09:17:27 +0000

    Final touches and clean ups

--- password_manager.py
+++ password_manager.py
@@ -7,49 +7,56 @@ username_list = ["BDSC2021"]
 
 
 #Functions
+def line():
+  print("     ")
+
 def age():
   while True:
     try:
       age = int(float(input("What is you age: ")))
       
-      if age < 10: 
-        print("You are not old enough")
+      if age < 13: 
+        print("Sorry you are not old enough")
         break
       elif age >= 13:
-        print("You are old enough please proceed")
+        print("Hello {}".format(name))
         menu()
+        break
     except ValueError:
-      print("Enter an integer not a word please: ")
+      print("Enter an integer not a word please")
 
 
 def menu():
   while True:
-    options = int(input("1: New User | 2: Existing User | 3: View Username List | 4: View Passwords List: "))
-    if options == 1:
-      add_user()
-      password()
-      break
-    elif options == 2:
-      log_in()
-      break
-    elif options == 3:
-      print(username_list)
-    elif options == 4:
-      print(password_list)
-      
-      break
-    else:
-      print("Please enter a number to the ones shown")
+    try:
+     options = int(input("1: New User | 2: Existing User | 3: View Username List | 4: View Passwords List | 5: Exit: "))
+     if options == 1:
+       line()
+       add_user()
+     elif options == 2:
+       log_in()
+     elif options == 3:
+       print(username_list)
+       menu()
+     elif options == 4:
+       print(password_list)
+       menu()
+    
+     elif options == 5:
+       break
+     else:
+      print("Please enter a number from the ones shown")
+    except ValueError:
+     print("Enter a number shown from the options given")
 
 def add_user():
   while True:
-
-    add_user = ("Enter a username to create a user for a new account: ")
-    x = len(add_user)
-    if x >= 5:
+    add_username = input(("Enter a username to create a user for a new account: "))
+    x = len(add_username)
+    if x >= 10:
       print("Username is created")
-      username_list.append(add_user)
-      break
+      username_list.append(add_username)
+      password()
     else:
       print("Username is too short, it must contain 5 or more letters")
 
@@ -57,10 +64,10 @@ def password():
   while True:
     add_password = input("Please enter a password, must be 8 letters or more: ")
     y = len(add_password)
-    if y >= 10:
+    if y >= 8:
       print("Password length is sufficent")
       password_list.append(add_password)
-      break
+      menu()
     else:
       print("Password is too short please enter one with 8 letters or more")
   
@@ -78,14 +85,16 @@ def log_in():
       menu()
     if password in password_list:
       add_extra = input("Welcome, Press 1 To View Your Lists, And Press 2 To Add More Passwords: ")
-    elif add_extra == 1:
+    if add_extra == 1:
       print(username_list)
       print(password_list)
       break
-    elif add_extra == 2:
+    if add_extra == 2:
       add_user()
       password()
     else:
       print("User was not found please try again")
 
+
+name = input("What is you name: ")
 age()
